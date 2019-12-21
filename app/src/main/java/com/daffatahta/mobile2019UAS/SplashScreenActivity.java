82a271f7f4e42e61a0e399859c3fcf7f9820c3dd
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2019-12-21 11:56:10 +0700

    Splashscreen Added

---
+++ app/src/main/java/com/daffatahta/mobile2019UAS/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.daffatahta.mobile2019UAS;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen conf = new EasySplashScreen(SplashScreenActivity.this)
+                .withBackgroundColor(Color.parseColor("#000000"))
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(3000)
+                .withLogo(R.mipmap.splashlogo)
+                .withFooterText("Daffa Tahta A 1741720152")
+                .withAfterLogoText("SplashScreen");
+
+        conf.getFooterTextView().setTextColor(Color.WHITE);
+        View v = conf.create();
+        setContentView(v);
+    }
+}
