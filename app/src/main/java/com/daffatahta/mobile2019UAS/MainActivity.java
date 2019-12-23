3df09ec29eb6d7b51e9268dee7c78fe1dbcf2a26
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2019-12-23 17:01:26 +0700

    timer logic added

--- app/src/main/java/com/daffatahta/mobile2019UAS/MainActivity.java
+++ app/src/main/java/com/daffatahta/mobile2019UAS/MainActivity.java
@@ -1,5 +1,6 @@
 package com.daffatahta.mobile2019UAS;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,8 +8,16 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.daffatahta.mobile2019UAS.fragments.about.About;
+import com.daffatahta.mobile2019UAS.fragments.alarm.Alarm;
+import com.daffatahta.mobile2019UAS.fragments.currency.Currency;
+import com.daffatahta.mobile2019UAS.fragments.temperature.Temperature;
+import com.daffatahta.mobile2019UAS.fragments.timer.Timer;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -26,6 +35,41 @@ public class MainActivity extends AppCompatActivity {
         //        R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         //drawer.addDrawerListener(toggle);
         //toggle.syncState();
+
+        NavigationView nav = findViewById(R.id.nav_view);
+        nav.setNavigationItemSelectedListener(this);
+
+        //if (savedInstanceState == null){
+        //    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+        //            new About()).commit();
+        //}
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_alarm :
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Alarm()).commit();
+                break;
+            case R.id.nav_timer :
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Timer()).commit();
+                break;
+            case R.id.nav_currency :
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Currency()).commit();
+                break;
+            case R.id.nav_temperature :
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Temperature()).commit();
+                break;
+            case R.id.nav_about :
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new About()).commit();
+                break;
+        }
+        return true;
     }
 
     @Override
--- app/src/main/java/com/daffatahta/mobile2019UAS/fragments/timer/Timer.java
+++ app/src/main/java/com/daffatahta/mobile2019UAS/fragments/timer/Timer.java
@@ -1,21 +1,42 @@
 package com.daffatahta.mobile2019UAS.fragments.timer;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.daffatahta.mobile2019UAS.R;
+
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Timer extends Fragment {
 
+    private EditText editText;
+    private Button setButton, startPause, reset;
+    private TextView countDown;
+
+    private Boolean timeRunning;
+
+    private long minuteStartInMillis;
+    private long minuteTimeLeftInMillis;
+    private long minuteEndInMillis;
+
+    private CountDownTimer countDownTimer;
 
     public Timer() {
         // Required empty public constructor
@@ -25,8 +46,185 @@ public class Timer extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+        //return inflater.inflate(R.layout.fragment_timer, container, false);
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        editText = view.findViewById(R.id.timerInput);
+        setButton = view.findViewById(R.id.buttonSetTimer);
+        startPause = view.findViewById(R.id.startButton);
+        reset = view.findViewById(R.id.reset);
+        countDown = view.findViewById(R.id.timeLeft);
+
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = editText.getText().toString();
+                if (input.length() == 0) {
+                    Toast.makeText(getContext(), "Mohon di isi!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                long miliSec = Long.parseLong(input) * 60000;
+                if (miliSec == 0) {
+                    Toast.makeText(getContext(), "masukkan angka positif", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                setTime(miliSec);
+                editText.setText("");
+            }
+        });
+
+        startPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (timeRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+
+
+        return view;
     }
 
+    private void setTime(long miliSec) {
+        minuteEndInMillis = miliSec;
+        resetTimer();
+        closeKeyboard();
+    }
+
+    private void startTimer() {
+        minuteEndInMillis = System.currentTimeMillis() + minuteTimeLeftInMillis;
+        countDownTimer = new CountDownTimer(minuteTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long l) {
+                minuteTimeLeftInMillis = l;
+                updateCountdownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeRunning = false;
+                updateWatchInterface();
+            }
+        }.start();
+        timeRunning = true;
+        updateWatchInterface();
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        timeRunning = false;
+        updateWatchInterface();
+    }
+
+    private void resetTimer() {
+        minuteTimeLeftInMillis = minuteStartInMillis;
+        updateCountdownText();
+        updateWatchInterface();
+    }
+
+    private void updateCountdownText() {
+        int hours = (int) (minuteTimeLeftInMillis / 1000) / 3500;
+        int minutes = (int) ((minuteTimeLeftInMillis / 1000) % 3600) / 60;
+        int seconds = (int) (minuteTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted;
+
+        if (hours > 0) {
+            timeLeftFormatted = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds);
+        } else {
+            timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        }
+        countDown.setText(timeLeftFormatted);
+    }
+
+    private void updateWatchInterface() {
+        if (timeRunning) {
+            editText.setVisibility(View.INVISIBLE);
+            setButton.setVisibility(View.INVISIBLE);
+            reset.setVisibility(View.INVISIBLE);
+            startPause.setText("Pause");
+        } else {
+            editText.setVisibility(View.VISIBLE);
+            setButton.setVisibility(View.VISIBLE);
+            startPause.setVisibility(View.VISIBLE);
+            if (minuteTimeLeftInMillis < 1000) {
+                startPause.setVisibility(View.VISIBLE);
+            } else {
+                startPause.setVisibility(View.VISIBLE);
+            }
+            if (minuteTimeLeftInMillis < minuteStartInMillis) {
+                reset.setVisibility(View.VISIBLE);
+            } else {
+                reset.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    private void closeKeyboard() {
+        View view = Timer.this.getView().findFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        SharedPreferences prefs = getActivity().getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putLong("StartTimeInMillisecs", minuteStartInMillis);
+        editor.putLong("MillisecindsLeft", minuteTimeLeftInMillis);
+        editor.putBoolean("TimeRunning", timeRunning);
+        editor.putLong("EndTIme", minuteTimeLeftInMillis);
+
+        editor.apply();
+
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        SharedPreferences prefs = getActivity().getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+        minuteStartInMillis = prefs.getLong("StartTimeInMillis", minuteTimeLeftInMillis);
+        minuteTimeLeftInMillis = prefs.getLong("MillisecLeft", minuteTimeLeftInMillis);
+        timeRunning = prefs.getBoolean("TimeRunning", false);
+
+        updateCountdownText();
+        updateWatchInterface();
+
+        if (timeRunning) {
+            minuteEndInMillis = prefs.getLong("endTime", 0);
+            minuteTimeLeftInMillis = minuteEndInMillis - System.currentTimeMillis();
+
+            if (timeRunning) {
+                minuteTimeLeftInMillis = 0;
+                timeRunning = false;
+                updateWatchInterface();
+                updateCountdownText();
+            } else {
+                startTimer();
+            }
+        }
+
+    }
 }
